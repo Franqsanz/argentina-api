d62cf88146768e0c38d427fc552daa4d507510ea
franco sanchez <franco_genta@hotmail.com>
2019-11-21 12:46:36 -0300

    push

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
+// const joi = require('@hapi/joi');
 
 const config = require('./config/config');
 const index = require('./routes/index');
@@ -15,17 +17,23 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// CORS
+app.use(
+  cors({
+    methods: 'GET, POST, OPTIONS, PUT, DELETE'
+  })
+);
 // configuracion de CORS para transportar la API
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Acces-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Methond'
-  );
-  res.header('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Acces-Control-Allow-Headers',
+//     'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Methond'
+//   );
+//   res.header('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 app.use(morgan('dev'));
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const products = require('../model/product');
 
