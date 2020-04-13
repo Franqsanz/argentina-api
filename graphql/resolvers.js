5b7d13b29516c1918cee28b5352fc5a21a3a310c
franco sanchez <franco_genta@hotmail.com>
2020-04-12 23:27:23 -0300

    remove de body parser y agregado de apollod-datasource-rest

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,18 @@
 /* eslint-disable prettier/prettier */
 'use strict';
 
+// const fetch = require('node-fetch');
+
 const resolvers = {
     Query: {
         provinces: () => provinces()
     }
 }
+
+// function fetchProvinces() {
+//     return fetch('http://localhost:33528/api/v1/provinces')
+//         .then(res => res.json())
+//         .then(data => data.provinces)
+// }
 
 module.exports = resolvers;
