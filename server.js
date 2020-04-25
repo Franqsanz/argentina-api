e841b8d9f4af0b1434876bd596abecc5927027b8
franco sanchez <franco_genta@hotmail.com>
2020-04-25 12:37:04 -0300

    eliminando graphql de la rama master

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 // const pagination = require('express-simple-pagination');
-// const { ApolloServer } = require('apollo-server-express');
 
 const config = require('./config/config');
 const provinces = require('./routes/provinces');
 require('./config/connectionDB');
-// const resolvers = require('./graphql/resolvers');
-// const typeDefs = require('./graphql/typeDefs');
-// const { ProvinceApi } = require('./graphql/sources');
 
 const app = express();
 
@@ -28,19 +24,7 @@ app.use(
     noSniff: true
   })
 );
-
 app.use(compression());
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   introspection: true,
-//   playground: true,
-//   dataSources: () => ({
-//     provincesApi: new ProvinceApi()
-//   })
-// });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -49,20 +33,15 @@ app.use(
     replaceWith: '_'
   })
 );
-
 // CORS
 app.use(cors());
 // Routes
 app.use('/api/v1', provinces);
-
-// server.applyMiddleware({ app });
 
 app.listen(config.Port, () =>
   console.log(`
   REST ➡  http://localhost:${config.Port}/api/v1/provinces
   `)
 );
-
-// GraphQL ➡  http://localhost:${config.Port}${server.graphqlPath}
 
 module.exports = app;
