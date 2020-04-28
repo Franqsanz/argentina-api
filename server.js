2fea0f07df89de8461ade4d64f409e8e6a7b934c
franco sanchez <franco_genta@hotmail.com>
2020-04-27 23:02:47 -0300

    graphql solucionado con node-fetch en la rama master

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 // const pagination = require('express-simple-pagination');
+const { ApolloServer } = require('apollo-server-express');
 
 const config = require('./config/config');
 const provinces = require('./routes/provinces');
 require('./config/connectionDB');
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
 
 const app = express();
 
@@ -25,6 +28,14 @@ app.use(
   })
 );
 app.use(compression());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,11 +43,14 @@ app.use(morgan('dev'));
 app.use(mongoSanitize({ replaceWith: '_' }));
 // Routes
 app.use('/api/v1', provinces);
-app.all('*', (req, res) => res.status(404).send('Page Not Found'));
+// app.all('*', (req, res) => res.status(404).send('Page Not Found'));
+
+server.applyMiddleware({ app });
 
 app.listen(config.Port, () =>
   console.log(`
   REST ➡  http://localhost:${config.Port}/api/v1/provinces
+  GraphQL ➡  http://localhost:${config.Port}${server.graphqlPath}
   `)
 );
 
